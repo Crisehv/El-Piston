a77b0dc6627bf25f2da5c1acf8617b65f6a2eb77
Marcos <Marcos@marcos>
2015-10-14 15:11:15 -0600

    Clase conexion agregada

---
+++ src/Modelos/Conexion.java
@@ -0,0 +1,25 @@
+package Modelos;
+import java.sql.*;
+public class Conexion {
+    
+    public static Connection conn; 
+    
+    public void conectar()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver"); 
+            conn = DriverManager.getConnection("jdbc:mysql://secure172.inmotionhosting.com:3306/nitrow5_tallerelpiston","nitrow5_umg","@E1umg"); 
+            System.out.println("Se ha conectado correctamente a la base de datos"); 
+        }catch(Exception e)
+        {
+             System.out.println(" NO se ha conectado correctamente a la base de datos " + e.getMessage()); 
+        }
+    }
+    
+    
+    public Connection obtenerConexion()
+    {
+        return conn; 
+    }
+}
