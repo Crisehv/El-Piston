319f9b8665c681498bb16930e763364e7be43dc5
herman <homlopes@hotmail.com>
2015-10-16 15:20:35 -0600

    modificación de controlador

--- Taller El Piston(ProyectoFinal)/src/Modelos/ClientesDAD.java
+++ Taller El Piston(ProyectoFinal)/src/Modelos/ClientesDAD.java
@@ -8,6 +8,8 @@ package Modelos;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -21,6 +23,25 @@ public class ClientesDAD {
     String telefono;
     
     
-    Connection Con;
-    ResultSet Consulta;
-}
+    Connection Conn;
+    Conexion Cone = new Conexion();
+    public void InsertarClientes(String dpiclientes, String apellidos, String nombre, String direccion, String telefono){
+        
+        this.dpiclientes= dpiclientes;
+        this.apellidos= apellidos;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        
+         try{
+            
+             JOptionPane.showMessageDialog(null, Cone.conectar());
+            Conn = Cone.obtenerConexion();
+            Statement Comando = Conn.createStatement();
+            Comando.executeUpdate("insert into clientes values( '"+dpiclientes+"', '"+apellidos+"', '"+nombre+"','"+direccion+"','"+telefono+"')");
+        }
+ catch(Exception E){
+            
+            JOptionPane.showMessageDialog(null, "Ocurrio un error al ingresar los datos..."+E);
+    }
+} }
